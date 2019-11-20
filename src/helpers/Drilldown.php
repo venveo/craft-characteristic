d115989fc5367a4516d4ed28d07b71be22427e96
Ransom <ransom@venveo.com>
2019-11-20 10:12:58 -0500

    Fix typo and fix sorting of answers

--- src/helpers/Drilldown.php
+++ src/helpers/Drilldown.php
@@ -55,7 +55,7 @@ class Drilldown extends Component
             ->asArray()
             ->all());
 
-        $values = CharacteristicValueElement::find()->id($valueIds)->fixedOrder(true);
+        $values = CharacteristicValueElement::find()->id($valueIds);
 
         return $values;
     }
