ccf8a57a9c753cadda0c83ccdce9bd039bbf12fe
Ransom <ransom@venveo.com>
2019-11-20 09:01:03 -0500

    Improve styling

--- src/assetbundles/characteristicsfield/src/CharacteristicItem.vue
+++ src/assetbundles/characteristicsfield/src/CharacteristicItem.vue
@@ -2,24 +2,23 @@
     <div class="characteristic-item matrixblock">
         <input type="hidden" :name="name + '[attribute]'" :value="attribute"/>
         <input type="hidden" :name="name + '[value]'" :value="value"/>
-        <div class="titlebar">
-            <div class="blocktype">Characteristic</div>
-        </div>
-        <div class="actions">
-            <a class="error icon delete" @click="$emit('delete')" title="Delete" role="button" data-icon="remove"></a>
-        </div>
-        <div class="fields flex">
+
+        <div class="fields flex flex-nowrap">
             <div class="input ltr">
                 <div class="select">
-                <select v-model="attribute">
-                    <option v-for="option in options" :key="option.id" :value="option.handle"
-                            :disabled="option.disabled">{{option.title}}
-                    </option>
-                </select>
+                    <select v-model="attribute">
+                        <option v-for="option in options" :key="option.id" :value="option.handle"
+                                :disabled="option.disabled">{{option.title}}
+                        </option>
+                    </select>
                 </div>
             </div>
             <div class="input ltr">
-                <input class="text" type="text" v-model="value">
+                <input class="text fullwidth" type="text" v-model="value">
+            </div>
+            <div class="actions">
+                <a class="error icon delete" @click="$emit('delete')" title="Delete" role="button"
+                   data-icon="remove"></a>
             </div>
         </div>
     </div>
@@ -59,6 +58,16 @@
 
 <style lang="scss" scoped>
     .characteristic-item.matrixblock {
+        padding-top: 10px;
+
+        .fields {
+            align-items: baseline;
+        }
+
+        .actions {
+            margin-left: auto;
+        }
+
         .actions > .delete {
             margin-right: 10px;
             font-size: 20px;
