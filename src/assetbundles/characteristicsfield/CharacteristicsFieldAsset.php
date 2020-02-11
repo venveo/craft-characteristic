0eeb15efda4f0f4eb853feea1664a53dbd6fa224
Ransom <ransom@venveo.com>
2020-02-11 11:37:15 -0500

    Started work on new frontend js

--- src/assetbundles/characteristicsfield/CharacteristicsFieldAsset.php
+++ src/assetbundles/characteristicsfield/CharacteristicsFieldAsset.php
@@ -12,22 +12,12 @@ namespace venveo\characteristic\assetbundles\characteristicsfield;
 
 use craft\web\AssetBundle;
 use craft\web\assets\cp\CpAsset;
-use craft\web\assets\vue\VueAsset;
 
 /**
  * Asset bundle for admin tables
  */
 class CharacteristicsFieldAsset extends AssetBundle
 {
-    /**
-     * @var bool
-     */
-    private $useDevServer = false;
-
-    /**
-     * @var bool
-     */
-    private $devServerBaseUrl = 'https://localhost:3000/';
 
     /**
      * @inheritdoc
@@ -37,25 +27,16 @@ class CharacteristicsFieldAsset extends AssetBundle
         $this->sourcePath = __DIR__ . '/dist/';
 
         $this->depends = [
-            CpAsset::class,
-            VueAsset::class,
+            CpAsset::class
         ];
 
-        if ($this->useDevServer) {
-            $this->js = [
-                $this->devServerBaseUrl . 'app.js',
-            ];
-        } else {
-            $this->css = [
-                'css/chunk-vendors.css',
-                'css/app.css',
-            ];
+        $this->css = [
+            'CharacteristicsInput.css',
+        ];
 
-            $this->js = [
-                'js/chunk-vendors.js',
-                'js/app.js',
-            ];
-        }
+        $this->js = [
+            'CharacteristicsInput.js',
+        ];
 
         parent::init();
     }
